57c9669e7dfc97963b6b07de4fec662532cae46d
Samuel Rezende <sam_profissional@outlook.com>
2026-03-02 17:01:32 -0300

    literals and types examples

---
+++ literalsAndTypes.py
@@ -0,0 +1,34 @@
+# Números octais são formas de representar um valor
+# Sempre precedidos por 0o (zero-o)
+print(0o123)
+
+# Números hexadecimais são formas de representar um valor
+# Sempre precedidos por 0x (zero-x)
+print(0x123)
+
+# Números flutuantes são representados com . entre os valores inteiros e decimais
+print(2.5)
+print(0.4)
+print(2.)
+print(.4)
+
+# Expoentes podem ser utilizados com E para representar a elevação de uma potência
+print(3e8)
+
+# O expoente (valor depois do E) tem que ser inteiro, a base (valor antes do E) pode ser um número flutuante ou inteiro
+print(6.62607E-34)
+
+# Python sempre escolhe a forma mais econômica de apresentação do número
+print(0.0000000000000000000001)
+
+# Formas simples de substituir caracteres reservados
+print("Eu gosto de \"Monty Python\"")
+print('Eu gosto "Monty Python"')
+print("I'm Monty Python.")
+
+# Valores booleanos
+print(True > False)
+print(True < False)
+
+#Literias em string
+print('"Eu sou"\n""aprendizado""\n"""Python"""')
