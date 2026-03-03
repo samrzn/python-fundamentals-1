2e8eb5b43b125bdc1970d8dbd2d3be044a172c2f
Samuel Rezende <sam_profissional@outlook.com>
2026-03-02 22:01:20 -0300

    code comment improvements

--- variables.py
+++ variables.py
@@ -1,57 +1,58 @@
-# # Variáveis
+# Variáveis
  
-# var = 1
-# print(var)
+var = 1
+print(var)
 
-# importMoreData = [1, 2, 3, 4]
-# print(importMoreData)
-# # Uma variável só existe a partir do momento que recebe um valor
-# var = 1
-# account_balance = 1000.0
-# client_name = 'John Doe'
-# print(var, account_balance, client_name)
-# print(var)
+importMoreData = [1, 2, 3, 4]
+print(importMoreData)
+# Uma variável só existe a partir do momento que recebe um valor
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
 
-# # Não é possível utilizar (declarar no código) uma variável que não existe
-# var = 1
-# # Declarar uma variável que não existe causa erro
-# # print(Var)
+# Não é possível utilizar (declarar no código) uma variável que não existe
+var = 1
+# Declarar uma variável que não existe causa erro
+# print(Var)
 
-# var = "3.8.5"
-# print("Versão Python: " + var)
+var = "3.8.5"
+print("Versão Python: " + var)
 
-# # Manipulando variáveis
-# john = 3
-# mary = 5
-# adam = 6
+# Manipulando variáveis
+john = 3
+mary = 5
+adam = 6
 
-# print(adam, mary, john, sep=", ")
+print(adam, mary, john, sep=", ")
 
-# total_apples = (adam + mary + john)
-# print("Número total de maças: " + str(total_apples))
+total_apples = (adam + mary + john)
+# Convertendo Int em String com a função str()
+print("Número total de maças: " + str(total_apples))
 
-# # Simplificando operações em Python
-# x = 1
-# sheep = 0
-# x *= 2
-# sheep += 1
-# print(x, sheep)
+# Simplificando operações em Python
+x = 1
+sheep = 0
+x *= 2
+sheep += 1
+print(x, sheep)
 
-# # Expressões e Operadores de Atalho
-# var = 10
-# rem = 1
-# j = 2
-# i = 3
-# i = i + 2 * j 
-# # i += 2 * j
-# var = var / 2 
-# # var /= 2
-# rem = rem % 10 
-# # rem %= 10
-# j = j - (i + var + rem) 
-# # j -= (i + var + rem)
-# x = x ** 2 
-# # x **= 2
+# Expressões e Operadores de Atalho
+var = 10
+rem = 1
+j = 2
+i = 3
+i = i + 2 * j 
+# i += 2 * j
+var = var / 2 
+# var /= 2
+rem = rem % 10 
+# rem %= 10
+j = j - (i + var + rem) 
+# j -= (i + var + rem)
+x = x ** 2 
+# x **= 2
 
 # Calcule a equação: 3x3 - 2x2 + 3x - 1
 x = float(3e3 - 2e2 + 3 - 1)
