f818ad1deeb1401ffacc23fd874b9a185cd50e76
Samuel Rezende <sam_profissional@outlook.com>
2026-03-20 18:17:46 -0300

    operations within arrays values

--- arraysManipulation.py
+++ arraysManipulation.py
@@ -111,3 +111,33 @@ my_list = [1, 'a', ["lista", 64, [0, 1], False]]
 print(my_list[2])  # outputs: ['lista', 64, [0, 1], False]
 print(my_list[2][2])  # outputs: [0, 1]
 print(my_list[2][2][1])  # outputs: 1
+
+# -----------------------------------------------
+
+# Copiando uma parte de um array usando :
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:3] # Copia os elementos da lista de índice 1 até o 2 (o índice 3 é excluído, pois não participa da fração)
+print(new_list) # start : end-1 = [8, 6]
+ 
+# Copiando uma parte de um array usando :, definindo um índice negativo para o final da lista
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:-1]
+print(new_list) # outputs: [8, 6, 4] (o índice -1 é o último elemento da lista, que é excluído da fração)
+ 
+# -----------------------------------------------
+
+# Operadores In e Not In para verificar a presença ou ausência de um elemento em uma lista
+my_list = [0, 3, 12, 8, 2]
+print(5 in my_list)
+print(5 not in my_list)
+print(12 in my_list)
+
+# Encontrando os números sorteados presentes na lista de apostas usando o operador In
+drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
+for number in bets:
+    if number in drawn:
+        hits += 1
+print(hits)
+
